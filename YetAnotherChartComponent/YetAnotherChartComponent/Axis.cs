5d45409d36409855f915394b28d9baac287e9c0d
eScape LLC <winker9@hotmail.com>
2017-09-08 12:15:04 -0400

    axis cwv.

--- YetAnotherChartComponent/YetAnotherChartComponent/Axis.cs
+++ YetAnotherChartComponent/YetAnotherChartComponent/Axis.cs
@@ -12,16 +12,37 @@ using Windows.UI.Xaml.Shapes;
 namespace eScapeLLC.UWP.Charts {
 	#region AxisCommon
 	/// <summary>
-	/// Consolidate common properties for axes.
+	/// Consolidate common properties for axes and implement IChartAxis.
 	/// </summary>
-	public abstract class AxisCommon : ChartComponent {
+	public abstract class AxisCommon : ChartComponent, IChartAxis {
 		#region properties
+		/// <summary>
+		/// The axis type.
+		/// </summary>
 		public AxisType Type { get; private set; }
+		/// <summary>
+		/// The axis orientation.
+		/// </summary>
 		public AxisOrientation Orientation { get; private set; }
+		/// <summary>
+		/// The lower limit or NaN if not initialized.
+		/// </summary>
 		public double Minimum { get; protected set; } = double.NaN;
+		/// <summary>
+		/// The upper limit or NaN if not initialized.
+		/// </summary>
 		public double Maximum { get; protected set; } = double.NaN;
+		/// <summary>
+		/// The axis range or NaN if limits were not initialized.
+		/// </summary>
 		public double Range { get { return double.IsNaN(Minimum) || double.IsNaN(Maximum) ? double.NaN : Maximum - Minimum; } }
+		/// <summary>
+		/// The PX width of the axis "line".
+		/// </summary>
 		public double AxisLineThickness { get; set; } = 2;
+		/// <summary>
+		/// The PX margin of the axis "line" from the edge of its bounds facing the data area.
+		/// </summary>
 		public double AxisMargin { get; set; } = 2;
 		/// <summary>
 		/// The brush for the axis "line".
@@ -45,13 +66,29 @@ namespace eScapeLLC.UWP.Charts {
 			Orientation = ao;
 		}
 		#endregion
+		#region public
+		/// <summary>
+		/// Reset the limits to NaN.
+		/// </summary>
+		public void ResetLimits() { Minimum = double.NaN; Maximum = double.NaN; Dirty = true; }
+		/// <summary>
+		/// Update limits based on given value.
+		/// </summary>
+		/// <param name="value">Value to "see".</param>
+		/// <returns>The "mapped" value. By default it's the identity.</returns>
+		public double For(double value) {
+			if (double.IsNaN(Minimum) || value < Minimum) { Minimum = value; Dirty = true; }
+			if (double.IsNaN(Maximum) || value > Maximum) { Maximum = value; Dirty = true; }
+			return value;
+		}
+		#endregion
 	}
 	#endregion
 	#region ValueAxis
 	/// <summary>
 	/// Value axis is a vertical axis that represents the "Y" coordinate.
 	/// </summary>
-	public class ValueAxis : AxisCommon, IChartAxis {
+	public class ValueAxis : AxisCommon {
 		static LogTools.Flag _trace = LogTools.Add("ValueAxis", LogTools.Level.Verbose);
 		#region properties
 		protected Path Segments { get; set; }
@@ -65,14 +102,6 @@ namespace eScapeLLC.UWP.Charts {
 			BindBrush(this, "Brush", Segments, Path.FillProperty);
 		}
 		#endregion
-		#region IChartAxis
-		public void ResetLimits() { Minimum = double.NaN; Maximum = double.NaN; Dirty = true; }
-		public double For(double value) {
-			if (double.IsNaN(Minimum) || value < Minimum) { Minimum = value; Dirty = true; }
-			if (double.IsNaN(Maximum) || value > Maximum) { Maximum = value; Dirty = true; }
-			return value;
-		}
-		#endregion
 		#region extensions
 		public override void Enter(IChartEnterLeaveContext icelc) {
 			icelc.Add(Segments);
@@ -81,13 +110,21 @@ namespace eScapeLLC.UWP.Charts {
 			icelc.Remove(Segments);
 		}
 		public override void Layout(IChartLayoutContext iclc) {
-			iclc.ClaimSpace(this, Side.Right, 100);
+			// TODO add space for "nominal" label width
+			var space = AxisMargin + AxisLineThickness + 48;
+			iclc.ClaimSpace(this, Side.Right, space);
 		}
 		public override void Render(IChartRenderContext icrc) {
 			if (!Dirty) return;
 			_trace.Verbose($"{Name} min:{Minimum} max:{Maximum} r:{Range}");
 			Geometry.Figures.Clear();
 			var pf = PathHelper.Rectangle(0, Minimum, AxisLineThickness, Maximum);
+			var start = Math.Round(Minimum);
+			var end = Math.Round(Maximum);
+			var incr = 1.0;
+			_trace.Verbose($"grid start:{start} end:{end} inc:{incr}");
+			for(var vx = start; vx <= end; vx += incr) {
+			}
 			Geometry.Figures.Add(pf);
 			Dirty = false;
 		}
@@ -103,7 +140,7 @@ namespace eScapeLLC.UWP.Charts {
 	/// <summary>
 	/// Horizontal Category axis.
 	/// </summary>
-	public class CategoryAxis : AxisCommon, IChartAxis {
+	public class CategoryAxis : AxisCommon {
 		static LogTools.Flag _trace = LogTools.Add("CategoryAxis", LogTools.Level.Verbose);
 		#region properties
 		protected Path Segments { get; set; }
@@ -117,14 +154,6 @@ namespace eScapeLLC.UWP.Charts {
 			BindBrush(this, "Brush", Segments, Path.FillProperty);
 		}
 		#endregion
-		#region IChartAxis
-		public void ResetLimits() { Minimum = double.NaN; Maximum = double.NaN; Dirty = true; }
-		public double For(double value) {
-			if (double.IsNaN(Minimum) || value < Minimum) { Minimum = value; Dirty = true; }
-			if (double.IsNaN(Maximum) || value > Maximum) { Maximum = value; Dirty = true; }
-			return value;
-		}
-		#endregion
 		#region extensions
 		public override void Enter(IChartEnterLeaveContext icelc) {
 			icelc.Add(Segments);
@@ -133,7 +162,9 @@ namespace eScapeLLC.UWP.Charts {
 			icelc.Remove(Segments);
 		}
 		public override void Layout(IChartLayoutContext iclc) {
-			iclc.ClaimSpace(this, Side.Bottom, 32);
+			// TODO add space for "nominal" text height
+			var space = AxisMargin + AxisLineThickness + 24;
+			iclc.ClaimSpace(this, Side.Bottom, space);
 		}
 		public override void Render(IChartRenderContext icrc) {
 			if (!Dirty) return;
