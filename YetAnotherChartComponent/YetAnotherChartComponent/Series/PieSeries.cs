8983e3be08fba74892185dc366891812c98812ae
eScape LLC <winker9@hotmail.com>
2018-02-18 17:24:53 -0500

    custom state pie series.

--- YetAnotherChartComponent/YetAnotherChartComponent/Series/PieSeries.cs
+++ YetAnotherChartComponent/YetAnotherChartComponent/Series/PieSeries.cs
@@ -18,13 +18,82 @@ namespace eScapeLLC.UWP.Charts {
 		const double RADIUS = 10.0;
 		#region SeriesItemState
 		/// <summary>
+		/// Isolate common features for the different implementations of item state.
+		/// </summary>
+		protected interface ISeriesItemCommon {
+			/// <summary>
+			/// Percentage.
+			/// </summary>
+			double Percent { get; set; }
+			/// <summary>
+			/// Label text.
+			/// </summary>
+			String Label { get; set; }
+			/// <summary>
+			/// Used for layout information.
+			/// </summary>
+			double PlacementAngle { get; set; }
+			/// <summary>
+			/// Create the item data.
+			/// </summary>
+			/// <returns>New instance.</returns>
+			ISeriesItem Unwrap();
+		}
+		/// <summary>
 		/// The item state.
 		/// </summary>
-		protected class SeriesItemState : ItemState<Path> {
-			internal double Percent { get; set; }
-			internal String Label { get; set; }
-			internal double PlacementAngle { get; set; }
+		protected class SeriesItemState : ItemState<Path>, ISeriesItemCommon {
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.Percent"/>.
+			/// </summary>
+			public double Percent { get; set; }
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.Label"/>.
+			/// </summary>
+			public String Label { get; set; }
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.PlacementAngle"/>.
+			/// </summary>
+			public double PlacementAngle { get; set; }
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.Unwrap"/>.
+			/// </summary>
+			public ISeriesItem Unwrap() {
+				var sis2 = new ISeriesItemValue[2];
+				sis2[0] = new ChannelItemState(Index, XValueIndex, XValueOffset, Value, PlacementAngle, RADIUS / 2, Element, 0);
+				sis2[1] = new ChannelItemState(Index, XValueIndex, XValueOffset, Percent, PlacementAngle, RADIUS / 2, Element, 1);
+				var sivc = new ItemStateMultiChannelCore(Index, XValueIndex, XValueOffset, sis2);
+				return sivc;
+			}
 			internal SeriesItemState(int idx, double xv, double xvo, double yv, Path ele, int ch) : base(idx, xv, xvo, yv, ele, ch) { }
+		}
+		/// <summary>
+		/// Custom state version.
+		/// </summary>
+		protected class SeriesItemState_Custom : ItemStateCustom<Path>, ISeriesItemCommon {
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.Percent"/>.
+			/// </summary>
+			public double Percent { get; set; }
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.Label"/>.
+			/// </summary>
+			public String Label { get; set; }
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.PlacementAngle"/>.
+			/// </summary>
+			public double PlacementAngle { get; set; }
+			/// <summary>
+			/// See <see cref="ISeriesItemCommon.Unwrap"/>.
+			/// </summary>
+			public ISeriesItem Unwrap() {
+				var sis2 = new ISeriesItemValue[2];
+				sis2[0] = new ChannelItemState_Custom(Index, XValueIndex, XValueOffset, Value, PlacementAngle, RADIUS / 2, CustomValue, Element, 0);
+				sis2[1] = new ChannelItemState_Custom(Index, XValueIndex, XValueOffset, Percent, PlacementAngle, RADIUS / 2, CustomValue, Element, 1);
+				var sivc = new ItemStateMultiChannelCore(Index, XValueIndex, XValueOffset, sis2);
+				return sivc;
+			}
+			internal SeriesItemState_Custom(int idx, double xv, double xvo, double yv, object cs, Path ele, int ch) : base(idx, xv, xvo, yv, cs, ele, ch) { }
 		}
 		/// <summary>
 		/// Wrapper for the channel items.
@@ -43,6 +112,24 @@ namespace eScapeLLC.UWP.Charts {
 			}
 			internal ChannelItemState(int idx, double xv, double xvo, double yv, double degs, double hr, Path ele, int ch)
 				: base(idx, xv, xvo, yv, ele, ch) { this.angle = degs * TO_RADS; this.hr = hr; }
+		}
+		/// <summary>
+		/// Custom version.
+		/// </summary>
+		protected class ChannelItemState_Custom : ItemStateCustomWithPlacement<Path> {
+			readonly double hr;
+			readonly double angle;
+			/// <summary>
+			/// Extract the geometry information and create placement.
+			/// Cannot derive it from the ArcSegment so store it in <see cref="angle"/>.
+			/// </summary>
+			/// <returns></returns>
+			protected override Placement CreatePlacement() {
+				var dir = new Point(Math.Cos(angle), Math.Sin(angle));
+				return new MidpointPlacement(new Point(dir.X * hr, dir.Y * hr), dir, hr);
+			}
+			internal ChannelItemState_Custom(int idx, double xv, double xvo, double yv, double degs, double hr, object cs, Path ele, int ch)
+				: base(idx, xv, xvo, yv, cs, ele, ch) { this.angle = degs * TO_RADS; this.hr = hr; }
 		}
 		#endregion
 		#region properties
@@ -79,7 +166,7 @@ namespace eScapeLLC.UWP.Charts {
 		/// <summary>
 		/// Data needed for current state.
 		/// </summary>
-		protected List<SeriesItemState> ItemState { get; set; }
+		protected List<ItemState<Path>> ItemState { get; set; }
 		/// <summary>
 		/// Internal legend items.
 		/// </summary>
@@ -116,18 +203,16 @@ namespace eScapeLLC.UWP.Charts {
 		/// Ctor.
 		/// </summary>
 		public PieSeries() {
-			ItemState = new List<SeriesItemState>();
+			ItemState = new List<ItemState<Path>>();
 			InternalLegendItems = new List<Legend>();
 		}
 		#endregion
 		#region helpers
-		IEnumerable<ISeriesItem> UnwrapItemState(IEnumerable<SeriesItemState> siss) {
-			foreach (var sis in siss) {
-				var sis2 = new ISeriesItemValue[2];
-				sis2[0] = new ChannelItemState(sis.Index, sis.XValueIndex, sis.XValueOffset, sis.Value, sis.PlacementAngle, RADIUS/2, sis.Element, 0);
-				sis2[1] = new ChannelItemState(sis.Index, sis.XValueIndex, sis.XValueOffset, sis.Percent, sis.PlacementAngle, RADIUS/2, sis.Element, 1);
-				var sivc = new ItemStateMultiChannelCore(sis.Index, sis.XValueIndex, sis.XValueOffset, sis2);
-				yield return sivc;
+		IEnumerable<ISeriesItem> UnwrapItemState(IEnumerable<ItemState<Path>> siss) {
+			foreach (var state in siss) {
+				if (state is ISeriesItemCommon sis) {
+					yield return sis.Unwrap();
+				}
 			}
 		}
 		/// <summary>
@@ -135,13 +220,15 @@ namespace eScapeLLC.UWP.Charts {
 		/// </summary>
 		void UpdateLegend() {
 			List<Legend> legend = new List<Legend>();
-			foreach(var sis in ItemState) {
-				var leg = new Legend() {
-					Title = sis.Label,
-					Fill = sis.Element.Style.Find<Brush>(Path.FillProperty),
-					Stroke = sis.Element.Style.Find<Brush>(Path.StrokeProperty)
-				};
-				legend.Add(leg);
+			foreach(var state in ItemState) {
+				if (state is ISeriesItemCommon sic) {
+					var leg = new Legend() {
+						Title = sic.Label,
+						Fill = state.Element.Style.Find<Brush>(Path.FillProperty),
+						Stroke = state.Element.Style.Find<Brush>(Path.StrokeProperty)
+					};
+					legend.Add(leg);
+				}
 			}
 			LegendChanged?.Invoke(this, new LegendDynamicEventArgs(InternalLegendItems, legend));
 			InternalLegendItems = legend;
@@ -181,9 +268,9 @@ namespace eScapeLLC.UWP.Charts {
 			};
 			return path;
 		}
-		class State : RenderState_ValueAndLabel<SeriesItemState, Path> {
+		class State : RenderState_ValueAndLabel<ItemState<Path>, Path> {
 			internal double totalv;
-			internal State(List<SeriesItemState> sis, Recycler<Path> rc, params BindingEvaluator[] bes) : base(sis, rc, bes[0], bes[1], bes[2], bes[3]) { }
+			internal State(List<ItemState<Path>> sis, Recycler<Path> rc, params BindingEvaluator[] bes) : base(sis, rc, bes[0], bes[1], bes[2], bes[3]) { }
 		}
 		object IDataSourceRenderer.Preamble(IChartRenderContext icrc) {
 			if (String.IsNullOrEmpty(ValuePath)) return null;
@@ -193,7 +280,7 @@ namespace eScapeLLC.UWP.Charts {
 			var paths = ItemState.Select(ms => ms.Element);
 			var recycler = new Recycler<Path>(paths, CreatePath);
 			Generator.Reset();
-			return new State(new List<SeriesItemState>(), recycler,
+			return new State(new List<ItemState<Path>>(), recycler,
 				null,
 				!String.IsNullOrEmpty(ValueLabelPath) ? new BindingEvaluator(ValueLabelPath) : null,
 				by,
@@ -215,29 +302,43 @@ namespace eScapeLLC.UWP.Charts {
 			if (path == null) return;
 			// start with empty geometry
 			path.Item2.Data = new PathGeometry();
-			st.itemstate.Add(new SeriesItemState(index, valuex, valuex, valuey, path.Item2, index) { Label = label });
+			if (st.byl == null) {
+				st.itemstate.Add(new SeriesItemState(index, valuex, valuex, valuey, path.Item2, index) { Label = label });
+			}
+			else {
+				var cs = st.byl.For(item);
+				st.itemstate.Add(new SeriesItemState_Custom(index, valuex, valuex, valuey, cs, path.Item2, index) { Label = label });
+			}
+		}
+		PathFigure CreateSegment(double startangle, double degs) {
+			var startx = Math.Cos(startangle * TO_RADS) * RADIUS;
+			var starty = Math.Sin(startangle * TO_RADS) * RADIUS;
+			var endx = Math.Cos((startangle + degs) * TO_RADS) * RADIUS;
+			var endy = Math.Sin((startangle + degs) * TO_RADS) * RADIUS;
+			_trace.Verbose($"{Name}.segment s:({startx:F2},{starty:F2}) e:({endx:F2},{endy:F2})");
+			var pf = new PathFigure() { StartPoint = new Point(0, 0), IsClosed = true };
+			var leg1 = new LineSegment() { Point = new Point(startx, starty) };
+			var seg = new ArcSegment() { Size = new Size(RADIUS, RADIUS), Point = new Point(endx, endy), IsLargeArc = degs >= 180, SweepDirection = SweepDirection.Clockwise };
+			pf.Segments.Add(leg1);
+			pf.Segments.Add(seg);
+			return pf;
 		}
 		void IDataSourceRenderer.RenderComplete(object state) {
 			var st = state as State;
 			var startangle = 0.0;
-			foreach (var sis in st.itemstate) {
-				sis.Percent = Math.Abs(sis.Value) / st.totalv;
-				// lay out geometry
-				var degs = sis.Percent*360;
-				var pg = sis.Element.Data as PathGeometry;
-				sis.PlacementAngle = startangle + degs / 2;
-				var startx = Math.Cos(startangle*TO_RADS) * RADIUS;
-				var starty = Math.Sin(startangle*TO_RADS) * RADIUS;
-				var endx = Math.Cos((startangle + degs)*TO_RADS) * RADIUS;
-				var endy = Math.Sin((startangle + degs)*TO_RADS) * RADIUS;
-				_trace.Verbose($"{Name}[{sis.Index}] {sis.Value} sa:{startangle:F2} degs:{degs:F2} s:({startx:F2},{starty:F2}) e:({endx:F2},{endy:F2})");
-				var pf = new PathFigure() { StartPoint = new Point(0, 0), IsClosed = true };
-				var leg1 = new LineSegment() { Point = new Point(startx, starty) };
-				var seg = new ArcSegment() { Size = new Size(RADIUS, RADIUS), Point = new Point(endx, endy), IsLargeArc = degs >= 180, SweepDirection = SweepDirection.Clockwise };
-				pf.Segments.Add(leg1);
-				pf.Segments.Add(seg);
-				pg.Figures.Add(pf);
-				startangle += degs;
+			foreach (var sta in st.itemstate) {
+				var percent = Math.Abs(sta.Value) / st.totalv;
+				var pg = sta.Element.Data as PathGeometry;
+				if (sta is ISeriesItemCommon sic) {
+					sic.Percent = percent;
+					// lay out geometry
+					var degs = sic.Percent * 360;
+					sic.PlacementAngle = startangle + degs / 2;
+					_trace.Verbose($"{Name}[{sta.Index}] {sta.Value} sa:{startangle:F2} degs:{degs:F2}");
+					var pf = CreateSegment(startangle, degs);
+					pg.Figures.Add(pf);
+					startangle += degs;
+				}
 			}
 		}
 		void IDataSourceRenderer.Postamble(object state) {
