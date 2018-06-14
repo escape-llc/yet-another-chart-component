bfa47f27097208fd24e11f07e894d74052e6de7e
eScape LLC <winker9@hotmail.com>
2018-06-14 08:52:52 -0400

    Update to show current child count.

--- YetAnotherChartComponent/eScape.Yacc.Demo/Pages/Chart3.xaml.cs
+++ YetAnotherChartComponent/eScape.Yacc.Demo/Pages/Chart3.xaml.cs
@@ -1,13 +1,33 @@
 ﻿using eScape.Core.Page;
 using System.Collections.Generic;
+using System.Threading;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Yacc.Demo.VM;
+using System;
 
 namespace Yacc.Demo.Pages {
 	public sealed partial class Chart3 : BasicPage {
 		public override string PageTitle => "Recycling";
+		public int CurrentChildCount { get; private set; }
+		Timer cctimer;
 		public Chart3() {
 			this.InitializeComponent();
+			cctimer = new Timer(Timer_Callback, this, 1000, 1000);
+		}
+		/// <summary>
+		/// Update the child count so the viewer can see how recycling levels out.
+		/// </summary>
+		/// <param name="ox"></param>
+		async void Timer_Callback(object ox) {
+			await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+				// the chart has one child, which is a Canvas
+				var child = VisualTreeHelper.GetChild(chart, 0);
+				// everything lives in the canvas on one level
+				CurrentChildCount = VisualTreeHelper.GetChildrenCount(child);
+				// tell x:Bind
+				Bindings.Update();
+			});
 		}
 		protected override object InitializeDataContext(NavigationEventArgs e) {
 			// start out with placeholders so chart "appears" empty and "scrolls" to the left
@@ -16,6 +36,17 @@ namespace Yacc.Demo.Pages {
 			var vm = new TimedObservationsVM(Dispatcher, list);
 			vm.ResetCounter();
 			return vm;
+		}
+		protected override void DataContextReleased(NavigatingCancelEventArgs ncea) {
+			base.DataContextReleased(ncea);
+			if (cctimer != null) {
+				try {
+					cctimer.Change(Timeout.Infinite, Timeout.Infinite);
+					cctimer.Dispose();
+				} finally {
+					cctimer = null;
+				}
+			}
 		}
 	}
 }
