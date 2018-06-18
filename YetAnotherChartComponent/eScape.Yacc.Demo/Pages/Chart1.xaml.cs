f17ac348a92c5682f7f9b32af79f8da2228fc459
eScape LLC <winker9@hotmail.com>
2018-06-18 11:57:49 -0400

    Add child counter to page.

--- YetAnotherChartComponent/eScape.Yacc.Demo/Pages/Chart1.xaml.cs
+++ YetAnotherChartComponent/eScape.Yacc.Demo/Pages/Chart1.xaml.cs
@@ -2,16 +2,35 @@
 using eScapeLLC.UWP.Charts;
 using System;
 using System.Diagnostics;
+using System.Threading;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using Yacc.Demo.VM;
 
 namespace Yacc.Demo.Pages {
 	public sealed partial class Chart1 : BasicPage {
 		public override string PageTitle => "Demo Chart";
+		public int CurrentChildCount { get; private set; }
+		Timer cctimer;
 
 		public Chart1() {
 			this.InitializeComponent();
+			cctimer = new Timer(Timer_Callback, this, 1000, 1000);
+		}
+		/// <summary>
+		/// Update the child count so the viewer can see how recycling levels out.
+		/// </summary>
+		/// <param name="ox"></param>
+		async void Timer_Callback(object ox) {
+			await Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+				// the chart has one child, which is a Canvas
+				var child = VisualTreeHelper.GetChild(chart, 0);
+				// everything lives in the canvas on one level
+				CurrentChildCount = VisualTreeHelper.GetChildrenCount(child);
+				// tell x:Bind
+				Bindings.Update();
+			});
 		}
 		protected override object InitializeDataContext(NavigationEventArgs e) {
 			var vm = new ObservationsVM(Dispatcher, new[] {
@@ -24,7 +43,17 @@ namespace Yacc.Demo.Pages {
 			});
 			return vm;
 		}
-
+		protected override void DataContextReleased(NavigatingCancelEventArgs ncea) {
+			base.DataContextReleased(ncea);
+			if (cctimer != null) {
+				try {
+					cctimer.Change(Timeout.Infinite, Timeout.Infinite);
+					cctimer.Dispose();
+				} finally {
+					cctimer = null;
+				}
+			}
+		}
 		private void Add_item_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) {
 			(DataContext as ObservationsVM).AddTail();
 		}
