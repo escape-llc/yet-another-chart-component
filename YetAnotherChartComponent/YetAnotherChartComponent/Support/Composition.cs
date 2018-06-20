f90faf782fb966bc278635bc67c012acb1cd4e33
eScape LLC <winker9@hotmail.com>
2018-06-20 08:14:54 -0400

    Refactor common v1 elements into separate class.  Add support for v4 APIs.

--- YetAnotherChartComponent/YetAnotherChartComponent/Support/Composition.cs
+++ YetAnotherChartComponent/YetAnotherChartComponent/Support/Composition.cs
@@ -4,6 +4,55 @@ using Windows.UI.Composition;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Hosting;
 
+namespace eScapeLLC.UWP.Charts {
+	/// <summary>
+	/// Static helpers for basic <see cref="Compositor"/> support, which is V1.
+	/// </summary>
+	public static class CompositorSupport {
+		/// <summary>
+		/// Create the <see cref="CompositionAnimationGroup"/>.
+		/// Doesn't check for CONTRACT.
+		/// </summary>
+		/// <param name="compositor"></param>
+		/// <param name="target">The animation target.</param>
+		/// <param name="duration">Duration of offset animation in MS.</param>
+		/// <returns>New instance.</returns>
+		public static ICompositionAnimationBase CreateOffsetAnimation(this Compositor compositor, String target, int duration) {
+			// Define Offset Animation for the Animation group
+			var offsetAnimation = compositor.CreateVector3KeyFrameAnimation();
+			offsetAnimation.Target = target;
+			offsetAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue");
+			offsetAnimation.Duration = TimeSpan.FromMilliseconds(duration);
+			var animationGroup = compositor.CreateAnimationGroup();
+			animationGroup.Add(offsetAnimation);
+			return animationGroup;
+		}
+		/// <summary>
+		/// Create the <see cref="CompositionAnimationGroup"/>.
+		/// Doesn't check for CONTRACT.
+		/// </summary>
+		/// <param name="compositor"></param>
+		/// <param name="fadein">true: 0..1; false: 1..0.</param>
+		/// <param name="duration">Duration of animation in MS.</param>
+		/// <returns>New instance.</returns>
+		public static ICompositionAnimationBase CreateOpacityAnimation(this Compositor compositor, bool fadein, int duration) {
+			// Define Opacity Animation for the Animation group
+			var animation = compositor.CreateScalarKeyFrameAnimation();
+			animation.Target = nameof(Visual.Opacity);
+			if(fadein) {
+				animation.InsertKeyFrame(0f, 0f);
+				animation.InsertKeyFrame(1f, 1f);
+			}
+			else {
+				animation.InsertKeyFrame(1f, 0f);
+			}
+			animation.Duration = TimeSpan.FromMilliseconds(duration);
+			var animationGroup = compositor.CreateAnimationGroup();
+			animationGroup.Add(animation);
+			return animation;
+		}
+	}
+}
 namespace eScapeLLC.UWP.Charts.UniversalApiContract.v3 {
 	/// <summary>
 	/// Static helpers for composition APIs.
@@ -56,49 +105,28 @@ namespace eScapeLLC.UWP.Charts.UniversalApiContract.v3 {
 		/// Get whether we have API support.
 		/// </summary>
 		public static bool IsSupported { get; } = ApiInformation.IsApiContractPresent(CONTRACT, VERSION);
-		/// <summary>
-		/// Globally set the duration of the offset animation.
-		/// </summary>
-		public static int OffsetAnimationDuration { get; set; } = 250;
-		#endregion
-		#region internal (doesn't check CONTRACT)
-		/// <summary>
-		/// Create the <see cref="CompositionAnimationGroup"/>.
-		/// Doesn't check for CONTRACT.
-		/// </summary>
-		/// <param name="compositor"></param>
-		/// <param name="offsetDuration">Duration of offset animation in MS.</param>
-		/// <returns>New instance.</returns>
-		private static CompositionAnimationGroup CreateAnimationGroup(Compositor compositor, int offsetDuration) {
-			// Define Offset Animation for the Animation group
-			var offsetAnimation = compositor.CreateVector3KeyFrameAnimation();
-			offsetAnimation.Target = nameof(Visual.Offset);
-			offsetAnimation.InsertExpressionKeyFrame(1.0f, "this.FinalValue");
-			offsetAnimation.Duration = TimeSpan.FromMilliseconds(offsetDuration);
-			var animationGroup = compositor.CreateAnimationGroup();
-			animationGroup.Add(offsetAnimation);
-			return animationGroup;
-		}
 		#endregion
 		#region external (MUST check CONTRACT)
 		/// <summary>
 		/// Attach implicit animations to given element.
 		/// Creates new instances of everything.
 		/// </summary>
-		/// <param name="uix"></param>
-		public static void AttachAnimations(UIElement uix) {
+		/// <param name="uix">Target element.</param>
+		/// <param name="duration">Duration in MS.</param>
+		public static void AttachAnimations(UIElement uix, int duration) {
 			if (!IsSupported) return;
 			var elementVisual = ElementCompositionPreview.GetElementVisual(uix);
 			var compositor = elementVisual.Compositor;
 			var elementImplicitAnimation = compositor.CreateImplicitAnimationCollection();
-			// Define trigger and animation that should play when the trigger is triggered.
-			elementImplicitAnimation[nameof(Visual.Offset)] = CreateAnimationGroup(compositor, OffsetAnimationDuration);
+			// Define trigger and animation to play when the trigger is fired.
+			var prop = nameof(Visual.Offset);
+			elementImplicitAnimation[prop] = compositor.CreateOffsetAnimation(prop, duration);
 			elementVisual.ImplicitAnimations = elementImplicitAnimation;
 		}
 		/// <summary>
 		/// Detach implicit animations from given element.
 		/// </summary>
-		/// <param name="uix"></param>
+		/// <param name="uix">Target element.</param>
 		public static void DetachAnimations(UIElement uix) {
 			if (!IsSupported) return;
 			var elementVisual = ElementCompositionPreview.GetElementVisual(uix);
@@ -133,27 +161,6 @@ namespace eScapeLLC.UWP.Charts.UniversalApiContract.v4 {
 		/// </summary>
 		public static int AnimationDuration { get; set; } = 3250;
 		#endregion
-		#region internal (doesn't check CONTRACT)
-		/// <summary>
-		/// Create the <see cref="CompositionAnimationGroup"/>.
-		/// Doesn't check for CONTRACT.
-		/// </summary>
-		/// <param name="compositor"></param>
-		/// <param name="fadein">true: 0..1; false: 1..0.</param>
-		/// <param name="duration">Duration of animation in MS.</param>
-		/// <returns>New instance.</returns>
-		private static ICompositionAnimationBase CreateAnimationGroup(Compositor compositor, bool fadein, int duration) {
-			// Define Opacity Animation for the Animation group
-			var animation = compositor.CreateScalarKeyFrameAnimation();
-			animation.Target = nameof(Visual.Opacity);
-			animation.InsertKeyFrame(0f, fadein ? 0f : 1f);
-			animation.InsertKeyFrame(1f, fadein ? 1f : 0f);
-			animation.Duration = TimeSpan.FromMilliseconds(duration);
-			var animationGroup = compositor.CreateAnimationGroup();
-			animationGroup.Add(animation);
-			return animation;
-		}
-		#endregion
 		#region external (MUST check CONTRACT)
 		/// <summary>
 		/// Attach implicit show/hide animations to given element.
@@ -164,9 +171,9 @@ namespace eScapeLLC.UWP.Charts.UniversalApiContract.v4 {
 			if (!IsSupported) return;
 			var elementVisual = ElementCompositionPreview.GetElementVisual(uix);
 			var compositor = elementVisual.Compositor;
-			var show = CreateAnimationGroup(compositor, true, AnimationDuration);
+			var show = compositor.CreateOpacityAnimation(true, AnimationDuration);
 			ElementCompositionPreview.SetImplicitShowAnimation(uix, show);
-			var hide = CreateAnimationGroup(compositor, false, AnimationDuration);
+			var hide = compositor.CreateOpacityAnimation(false, AnimationDuration);
 			ElementCompositionPreview.SetImplicitHideAnimation(uix, hide);
 		}
 		/// <summary>
@@ -178,6 +185,14 @@ namespace eScapeLLC.UWP.Charts.UniversalApiContract.v4 {
 			var elementVisual = ElementCompositionPreview.GetElementVisual(uix);
 			ElementCompositionPreview.SetImplicitShowAnimation(uix, null);
 			ElementCompositionPreview.SetImplicitHideAnimation(uix, null);
+		}
+		/// <summary>
+		/// Enable translation.
+		/// </summary>
+		/// <param name="uix"></param>
+		public static void EnableTranslation(UIElement uix) {
+			if (!IsSupported) return;
+			ElementCompositionPreview.SetIsTranslationEnabled(uix, true);
 		}
 		#endregion
 	}
