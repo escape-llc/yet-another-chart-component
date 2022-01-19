8939632759df3e9e7680ee2c05fa1d925c224179
eScape LLC <winker9@hotmail.com>
2022-01-19 09:12:50 -0500

    update how data is staged.

--- YetAnotherChartComponent/eScape.Yacc.Demo/Pages/Chart8.xaml.cs
+++ YetAnotherChartComponent/eScape.Yacc.Demo/Pages/Chart8.xaml.cs
@@ -23,52 +23,73 @@ namespace Yacc.Demo.Pages {
 		/// <param name="ox"></param>
 		protected override object InitializeDataContext(NavigationEventArgs e) {
 			// start out with placeholders so chart "appears" empty and "scrolls" to the left
-			var g1 = new List<GroupInfo> {
+			var colgroups = new List<GroupInfo> {
 				new GroupInfo(0, "GroupC1"),
 				new GroupInfo(1, "GroupC2"),
 				new GroupInfo(2, "GroupC3"),
 				new GroupInfo(3, "GroupC4"),
-				new GroupInfo(4, "GroupC5")
+				new GroupInfo(4, "GroupC5"),
+				new GroupInfo(5, "GroupC6")
 			};
-			var g2 = new List<GroupInfo> {
+			var rowgroups = new List<GroupInfo> {
 				new GroupInfo(0, "GroupR1"),
 				new GroupInfo(1, "GroupR2"),
 				new GroupInfo(2, "GroupR3"),
 				new GroupInfo(3, "GroupR4"),
-				new GroupInfo(4, "GroupR5")
+				new GroupInfo(4, "GroupR5"),
+				new GroupInfo(5, "GroupR6"),
+				new GroupInfo(6, "GroupR7")
 			};
 			var list = new List<ObservationGrouped> {
-				new ObservationGrouped(g1[0].Label, g2[0].Label, 0, 0, 2),
-				new ObservationGrouped(g1[0].Label, g2[1].Label, 0, 1, 6),
-				new ObservationGrouped(g1[0].Label, g2[2].Label, 0, 2, 3),
-				new ObservationGrouped(g1[0].Label, g2[3].Label, 0, 3, 1),
-				new ObservationGrouped(g1[0].Label, g2[4].Label, 0, 4, 5),
+				new ObservationGrouped(colgroups[0].Label, rowgroups[0].Label, 0, 0, 2),
+				new ObservationGrouped(colgroups[0].Label, rowgroups[1].Label, 0, 1, 6),
+				new ObservationGrouped(colgroups[0].Label, rowgroups[2].Label, 0, 2, 3),
+				new ObservationGrouped(colgroups[0].Label, rowgroups[3].Label, 0, 3, 1),
+				new ObservationGrouped(colgroups[0].Label, rowgroups[4].Label, 0, 4, 5),
+				new ObservationGrouped(colgroups[0].Label, rowgroups[5].Label, 0, 5, 5),
+				new ObservationGrouped(colgroups[0].Label, rowgroups[6].Label, 0, 6, 6),
 
-				new ObservationGrouped(g1[1].Label, g2[0].Label, 1, 0, 2),
-				new ObservationGrouped(g1[1].Label, g2[1].Label, 1, 1, 7),
-				new ObservationGrouped(g1[1].Label, g2[2].Label, 1, 2, 3),
-				new ObservationGrouped(g1[1].Label, g2[3].Label, 1, 3, 1),
-				new ObservationGrouped(g1[1].Label, g2[4].Label, 1, 4, 5),
+				new ObservationGrouped(colgroups[1].Label, rowgroups[0].Label, 1, 0, 2),
+				new ObservationGrouped(colgroups[1].Label, rowgroups[1].Label, 1, 1, 7),
+				new ObservationGrouped(colgroups[1].Label, rowgroups[2].Label, 1, 2, 3),
+				new ObservationGrouped(colgroups[1].Label, rowgroups[3].Label, 1, 3, 1),
+				new ObservationGrouped(colgroups[1].Label, rowgroups[4].Label, 1, 4, 5),
+				new ObservationGrouped(colgroups[1].Label, rowgroups[5].Label, 1, 5, 5),
+				new ObservationGrouped(colgroups[1].Label, rowgroups[6].Label, 1, 6, 0),
 
-				new ObservationGrouped(g1[2].Label, g2[0].Label, 2, 0, 4),
-				new ObservationGrouped(g1[2].Label, g2[1].Label, 2, 1, 7),
-				new ObservationGrouped(g1[2].Label, g2[2].Label, 2, 2, 13),
-				new ObservationGrouped(g1[2].Label, g2[3].Label, 2, 3, 12),
-				new ObservationGrouped(g1[2].Label, g2[4].Label, 2, 4, 5),
+				new ObservationGrouped(colgroups[2].Label, rowgroups[0].Label, 2, 0, 4),
+				new ObservationGrouped(colgroups[2].Label, rowgroups[1].Label, 2, 1, 7),
+				new ObservationGrouped(colgroups[2].Label, rowgroups[2].Label, 2, 2, 13),
+				new ObservationGrouped(colgroups[2].Label, rowgroups[3].Label, 2, 3, 12),
+				new ObservationGrouped(colgroups[2].Label, rowgroups[4].Label, 2, 4, 5),
+				new ObservationGrouped(colgroups[2].Label, rowgroups[5].Label, 2, 5, 5),
+				new ObservationGrouped(colgroups[2].Label, rowgroups[6].Label, 2, 6, 9),
 
-				new ObservationGrouped(g1[3].Label, g2[0].Label, 3, 0, 11),
-				new ObservationGrouped(g1[3].Label, g2[1].Label, 3, 1, 7),
-				new ObservationGrouped(g1[3].Label, g2[2].Label, 3, 2, 13),
-				new ObservationGrouped(g1[3].Label, g2[3].Label, 3, 3, 10),
-				new ObservationGrouped(g1[3].Label, g2[4].Label, 3, 4, 9),
+				new ObservationGrouped(colgroups[3].Label, rowgroups[0].Label, 3, 0, 11),
+				new ObservationGrouped(colgroups[3].Label, rowgroups[1].Label, 3, 1, 7),
+				new ObservationGrouped(colgroups[3].Label, rowgroups[2].Label, 3, 2, 13),
+				new ObservationGrouped(colgroups[3].Label, rowgroups[3].Label, 3, 3, 10),
+				new ObservationGrouped(colgroups[3].Label, rowgroups[4].Label, 3, 4, 9),
+				new ObservationGrouped(colgroups[3].Label, rowgroups[5].Label, 3, 5, 5),
+				new ObservationGrouped(colgroups[3].Label, rowgroups[6].Label, 3, 6, 6),
 
-				new ObservationGrouped(g1[4].Label, g2[0].Label, 4, 0, 0),
-				new ObservationGrouped(g1[4].Label, g2[1].Label, 4, 1, 6),
-				new ObservationGrouped(g1[4].Label, g2[2].Label, 4, 2, 3),
-				new ObservationGrouped(g1[4].Label, g2[3].Label, 4, 3, 8),
-				new ObservationGrouped(g1[4].Label, g2[4].Label, 4, 4, 9),
+				new ObservationGrouped(colgroups[4].Label, rowgroups[0].Label, 4, 0, 0),
+				new ObservationGrouped(colgroups[4].Label, rowgroups[1].Label, 4, 1, 6),
+				new ObservationGrouped(colgroups[4].Label, rowgroups[2].Label, 4, 2, 3),
+				new ObservationGrouped(colgroups[4].Label, rowgroups[3].Label, 4, 3, 8),
+				new ObservationGrouped(colgroups[4].Label, rowgroups[4].Label, 4, 4, 9),
+				new ObservationGrouped(colgroups[4].Label, rowgroups[5].Label, 4, 5, 5),
+				new ObservationGrouped(colgroups[4].Label, rowgroups[6].Label, 4, 6, 6),
+
+				new ObservationGrouped(colgroups[5].Label, rowgroups[0].Label, 5, 0, 3),
+				new ObservationGrouped(colgroups[5].Label, rowgroups[1].Label, 5, 1, 1),
+				new ObservationGrouped(colgroups[5].Label, rowgroups[2].Label, 5, 2, 8),
+				new ObservationGrouped(colgroups[5].Label, rowgroups[3].Label, 5, 3, 2),
+				new ObservationGrouped(colgroups[5].Label, rowgroups[4].Label, 5, 4, 11),
+				new ObservationGrouped(colgroups[5].Label, rowgroups[5].Label, 5, 5, 2),
+				new ObservationGrouped(colgroups[5].Label, rowgroups[6].Label, 5, 6, 6),
 			};
-			var vm = new ObservationGroupedVM(Dispatcher, g1, g2, list);
+			var vm = new ObservationGroupedVM(Dispatcher, colgroups, rowgroups, list);
 			return vm;
 		}
 	}
